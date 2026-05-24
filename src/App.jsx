d77c8ac112ad3356993815b68b0faa9c2b743335
Justin Perdew <justjustin0911@gmail.com>
2026-05-23 22:38:56 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       </h1>
       <p className="text-lg text-red-900">
         I want to try to test out some of the tail-wind classes as it seems a interesting way to style my components.
-        I have been using CSS for a while and I am not sure if I want to switch to tail-wind or not, but I will give it a try and see how it goes.
+        I have been using CSS for a while and I am not sure if I want to switch to tail-wind or not, but I will give it a try and see how it goes. Yes!
       </p>
       <input type={"text"} className="border-2 border-gray-300 rounded-md p-4 m-4" placeholder="Enter a song!" />
       <div className="flex justify-center">
